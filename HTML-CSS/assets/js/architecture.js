bfa162a0404c18a8eda91cbdd3847ca59ae588ee
Audrey Wagner <49270523+audreywagner@users.noreply.github.com>
2019-10-17 18:49:08 +0200

    Add files via upload
    Ajout partie design (HTML - CSS - JS)

---
+++ HTML-CSS/assets/js/architecture.js
@@ -0,0 +1,36 @@
+/*
+    Style    
+*/
+var ThemeButton = document.getElementById('ChangeThemeButton');
+var IconThemeButton = document.getElementById('IconThemeButton');
+
+ThemeButton.addEventListener("click", switchCss);
+
+
+if (window.localStorage.theme == 'Night') {
+    changeTheme('Night');
+} else {
+    window.localStorage.theme == 'Day';
+    changeTheme('Day');
+}
+
+
+function changeTheme(newTheme) {    
+    document.getElementById('ThemeStylesheet').setAttribute('href', './assets/css/themes/' + newTheme + '.css');
+    window.localStorage.theme = newTheme;
+
+    if (window.localStorage.theme == 'Day') {
+        IconThemeButton.classList = 'fa fa-moon';
+    } else {
+        IconThemeButton.classList = 'fa fa-sun';
+    }
+} //changeTheme()
+
+
+function switchCss() {
+    if (window.localStorage.theme == 'Day') {
+        changeTheme('Night');
+    } else {
+        changeTheme('Day');
+    }
+} // switchCss()
