76deb7c6e6f45cd60acdca6286e7f55e17f113bb
Laurent <laurent-vouriot@etu.univ-amu.fr>
2019-10-19 23:23:16 +0200

    verification du token lors de l'arrivée sur la page de nouveau mdp

--- view/generatePwdV.php
+++ view/generatePwdV.php
@@ -1,3 +1,9 @@
 <?php
 
+    require(../generatePwdM.php);
+
+    $i_token = $_GET['token'];
+    if($i_token == null || !verifToken($i_token))
+      header('Location : accesInterdit.html');
+
     echo '<h1> ok </h1>';
