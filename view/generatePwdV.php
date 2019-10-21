9a0b64ae9e9d9687ba052eb103e21ee7600ee962
Laurent <laurent-vouriot@etu.univ-amu.fr>
2019-10-21 21:45:45 +0200

    debug new pwd

--- view/generatePwdV.php
+++ view/generatePwdV.php
@@ -3,11 +3,12 @@
 
        $i_token = $_GET['token'];
 
+       // ?token=<?=$i_token?
 ?>
 
 <main>
   <h1> nouveau mdp </h1>
-  <from  action="../controller/generatePwdC.php?token=<?=$i_token?>" method="post">
+  <from  action="../controller/generatePwdC.php" method="post">
     <input type="password" name="newPwd" placeholder="nouveau mot de passe"><br>
     <p> Confirmez votre mot de passe <br></p>
     <input type="password" name="confMdp" placeholder="confirmer mot de passe"><br>
