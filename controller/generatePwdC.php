868f8d7f36b0312db59b2cf75060ebbcfe31590d
Laurent <laurent-vouriot@etu.univ-amu.fr>
2019-10-21 20:22:24 +0200

    debug change pwd

--- controller/generatePwdC.php
+++ controller/generatePwdC.php
@@ -9,7 +9,7 @@
     if($i_token == '')
       header('Location : ../accesInterdit.html');
     // on verifie si le token existe  dans la bd et on redirige en fonction
-    else if(|| $i_token == null || !verifToken($i_token) ) {
+    else if($i_token == NULL || !verifToken($i_token) ) {
       header('Location : ../accesInterdit.html');
     }
 
