9589980db89823b6a9cdd5cebb18e39f437cd109
Laurent <laurent-vouriot@etu.univ-amu.fr>
2019-10-21 21:51:10 +0200

    debug new pwd

--- view/generatePwdV.php
+++ view/generatePwdV.php
@@ -9,7 +9,7 @@
 <main>
   <h1> nouveau mdp </h1>
   <from  action="../controller/generatePwdC.php" method="post">
-    <input type="password" name="newPwd" placeholder="nouveau mot de passe"><br>
+    <input type="text" name="newPwd" placeholder="nouveau mot de passe"><br>
     <p> Confirmez votre mot de passe <br></p>
     <input type="password" name="confMdp" placeholder="confirmer mot de passe"><br>
     <input type="submit" name="action" value="envoyer"><br>
