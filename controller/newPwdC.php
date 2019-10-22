fb9670a406d88fc6372802e1acef7bbc8abb775e
Laurent <laurent-vouriot@etu.univ-amu.fr>
2019-10-22 11:26:15 +0200

    messages d'erreurs

--- controller/newPwdC.php
+++ controller/newPwdC.php
@@ -17,7 +17,7 @@
     * Laurent
     * Envoie un mail avec le token généré à l'utilisateur souhaitant changer son mot de passe
     * in : string mail de user
-    */  
+    */
     function sendMail($s_mail) {
 
       $i_token  = generateToken();
@@ -29,7 +29,7 @@
       $s_msg .= '<h1 style="color : #ff793f">Mot de passe oublié ? </h1>';
       $s_msg .= '<h2 style="color : #ff793f">pas de panique !</h2>';
       $s_msg .= '<p> Dirigez vous sur ce lien : <br>';
-      $s_msg .= 'http://projet-iut-info.alwaysdata.net/mdpoublie/PROJET-PHP/view/generatePwdV.php?token=' . $i_token . '<br><br>';
+      $s_msg .= 'http://projet-iut-info.alwaysdata.net/mdpoublie/PROJET-PHP/view/generatePwdV.php?token=' . $i_token . 'step=errconf <br><br>';
       $s_msg .= 'Si vous n\'êtes pas à l\'origine de ce changement de mot de passe ignorez ce mail. <br><br>';
       $s_msg .= 'Faites attention peut-être que quelqu\'un essaie de vous pirater<br><p>';
       $s_msg .= '</body></html>';
