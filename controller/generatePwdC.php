46b7fd363cb281104a01a0b606fb646a01216c3f
Laurent <laurent-vouriot@etu.univ-amu.fr>
2019-10-22 23:03:29 +0200

    debug change pwd

--- controller/generatePwdC.php
+++ controller/generatePwdC.php
@@ -12,9 +12,10 @@
 
     if($s_newMdp != $s_confMdp)
        header('Location: ../view/generatePwdV.php?step=errconf');
-    else if(verifToken($i_token) && $s_step = 'ok')
+    else if(verifToken($i_token) && $s_step = 'ok'){
        changePwd($i_token,$s_newPwd);
        header('Location: ../view/generatePwdV.php?step=mdp');
+    }
     else
        header('Location: ../view/generatePwdV.php?step=errtkn');
 
