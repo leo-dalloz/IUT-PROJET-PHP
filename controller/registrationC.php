7f74595a6a0c7d2892cb456b0ccc3ea3f176e65f
Audrey Wagner <49270523+audreywagner@users.noreply.github.com>
2019-10-23 01:10:39 +0200

    Modification registrationC ajout condition +13ans

--- controller/registrationC.php
+++ controller/registrationC.php
@@ -23,11 +23,23 @@
 
         require '../Model/registrationM.php';
 
-        if ($s_surname != NULL AND $s_name != NULL AND $s_pseudo != NULL AND $s_email != NULL AND $d_birth != NULL AND $s_pwd != NULL AND $s_pwd2 != NULL AND $s_gender != NULL AND checkPseudo($s_pseudo) == 0 AND checkEmail($s_email) == 0)
+        // Check si l'utilisateur a plus de 13 ans
+        $d_actualdate = new DateTime(date("Y-m-d"));   
+        $d_postdate = new DateTime($_POST['birthday']);
+        if (($d_actualdate->diff($d_postdate)->format('%y') < 13)) {
+            header('Location: ../View/registrationV.php?error=birthday');
+        }           
+
+        if ($s_surname != NULL AND 
+            $s_name    != NULL AND 
+            $s_pseudo  != NULL AND 
+            $s_email   != NULL AND 
+            $d_birth   != NULL AND 
+            $s_pwd     != NULL AND 
+            $s_pwd2    != NULL AND 
+            $s_gender  != NULL AND 
+            checkPseudo($s_pseudo) == 0 AND checkEmail($s_email) == 0)
         {
-
-
-
             $s_pwd = password_hash($s_pwd, PASSWORD_DEFAULT);
 
             $newUser = new User(0,$s_surname, $s_name, $s_pseudo, $s_email, $d_birth, $s_pwd, $s_gender);
@@ -35,9 +47,17 @@
             registration($newUser);
 
             header('Location: ../View/loginV.php');
-
         }
-        else if ($s_surname == NULL OR $s_name == NULL OR $s_pseudo == NULL OR $s_email == NULL OR $d_birth == NULL OR $s_pwd == NULL OR $s_pwd2 == NULL OR $s_gender == NULL)
+
+
+        else if ($s_surname == NULL OR 
+                 $s_name    == NULL OR 
+                 $s_pseudo  == NULL OR 
+                 $s_email   == NULL OR 
+                 $d_birth   == NULL OR 
+                 $s_pwd     == NULL OR 
+                 $s_pwd2    == NULL OR 
+                 $s_gender  == NULL)
         {
             header('Location: ../View/registrationV.php?error=wrong');
         }
