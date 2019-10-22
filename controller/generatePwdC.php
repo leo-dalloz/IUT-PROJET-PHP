40250e5087cd44064748b02ed1825a30ea934a54
Laurent <laurent-vouriot@etu.univ-amu.fr>
2019-10-22 11:39:40 +0200

    messages d'erreurs

--- controller/generatePwdC.php
+++ controller/generatePwdC.php
@@ -10,7 +10,6 @@
 
 
     if($s_newMdp == $s_confMdp)
-       header('Location :../view/generatePwdV.php?token="'. $i_token . '?step=errconf"');
-
-       
-    changePwd($i_token,$s_newPwd);
+       header('Location :../view/generatePwdV.php?token="'. $i_token . '&step=errconf"');
+    else
+      changePwd($i_token,$s_newPwd);
