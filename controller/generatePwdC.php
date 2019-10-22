1703ef67c209e09f952eeae14bb61bb5cf5b8761
Laurent <laurent-vouriot@etu.univ-amu.fr>
2019-10-22 22:22:21 +0200

    debug change pwd

--- controller/generatePwdC.php
+++ controller/generatePwdC.php
@@ -8,12 +8,11 @@
     $s_confPwd = $_POST['confMdp'];
 
 
+    require ('../view/generatePwdV.php?step=ok&token='. $i_token);
 
     if($s_newMdp != $s_confMdp)
        header('Location: ../view/generatePwdV.php?step=errconf');
-    else {
+    else if() {
        changePwd($i_token,$s_newPwd);
        header('Location: ../view/generatePwdV.php?step=mdp');
     }
-
-    require ('../view/generatePwdV.php?step=ok&token='. $i_token);
