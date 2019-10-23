36fa4c5a4d2f6529b1db263a3cb1e8b2e6a580c0
Laurent <laurent-vouriot@etu.univ-amu.fr>
2019-10-23 19:10:53 +0200

    test si le token existe ou si les 2 MDP entrés correspondent

--- controller/generatePwdC.php
+++ controller/generatePwdC.php
@@ -1,16 +1,28 @@
 <?php
     require ('../model/generatePwdM.php');
 
+    if(!isset($_GET['token']) || !verifToken($i_token))
+      header ("Loaction : accesInterdit.html");
+
+
 
     $i_token  = $_GET['token'];
 
-    if(isset($_POST['newPwd'])){
-       $i_token  = $_GET['token'];
-       $s_newPwd = $_POST['newPwd'];
+    if(isset($_POST['newPwd'])
+    && isset($_POST['confPwd']))
+    {
 
-       echo "testtestestesttestds " . $i_token ;
-       verifToken($i_token);
-       echo 'wsh';
-       // changePwd($i_token,$s_newPwd);
+       $i_token   = $_GET['token'];
+
+       $s_newPwd  = $_POST['newPwd'];
+       $s_confPwd = $_POST['confPwd'];
+
+       if($s_newPwd != $s_confPwd)
+          header("Location : ../view/generatePwdV.php?step=errconf");
+
+
+       changePwd($i_token,$s_newPwd);
+
+       header("Location : ../view/generatePwdV.php?step=mdp");
     }
     require ('../view/generatePwdV.php');
--- view/generatePwdV.php
+++ view/generatePwdV.php
@@ -5,8 +5,6 @@
     $s_msg = 'les 2 mots de passes entrés sont differenets';
   else if($step = 'mpd')
     $s_msg ='mot de passe modifié';
-  else if ($step = 'errtkn')
-    $s_msg = 'erreur de token';
   else
     $s_msg ='bonjour';
 ?>
@@ -16,7 +14,7 @@
   <form  action="../controller/generatePwdC.php?token=<?= $i_token ?>"  method="post">
     <input type="text" name="newPwd" placeholder="nouveau mot de passe"><br>
     <p> Confirmez votre mot de passe <br></p>
-    <input type="password" name="confMdp" placeholder="confirmer mot de passe"><br>
+    <input type="password" name="confPwd" placeholder="confirmer mot de passe"><br>
     <input type="submit" name="action" value="envoyer"><br>
     <?= $s_msg ?>
   </form>
