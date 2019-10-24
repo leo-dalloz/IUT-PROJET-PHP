e0f4a58df9ccc4b7a6d4a50fe36371451a4b0b8f
Laurent <laurent-vouriot@etu.univ-amu.fr>
2019-10-24 16:19:59 +0200

    affichage de message suivant le step

--- controller/generatePwdC.php
+++ controller/generatePwdC.php
@@ -2,14 +2,23 @@
     require ('../model/generatePwdM.php');
 
 
+    /*
+      si il n'y pas de token c'est que quelqu'un essaye d'acceder à la page de
+      modification de passe sans avoir reçu de mail
+    */
     if(empty($_GET['token']))
         header('Location: accesInterdit.html');
 
 
     $i_token   = $_GET['token'];
-
+    /*
+      si le token n'existe oas dans la bd c'est que la personne essaye d'acceder à
+      la page avec un token au hasard. pas sympa
+    */
     if(!verifToken($i_token))
       header('Location: accesInterdit.html');
+
+
 
 
     if(isset($_POST['newPwd'])
--- view/generatePwdV.php
+++ view/generatePwdV.php
@@ -3,24 +3,24 @@
   $step    = $_GET['step'];
 
   if ($step == 'hello'){
-    $s_msg = 'bonjour';
+    $s_msg = 'veuillez entrer votre nouveau mot de passe';
   }
   else if ($step == 'errconf') {
     $s_msg = 'les 2 mots de passes entrés sont differenets';
     $i_token = $_GET['token'];
   }
-  else if($step == 'mpd') {
+  else if($step == 'mdp') {
     $s_msg = 'mot de passe modifié';
   }
 ?>
 
 <main>
   <h1> nouveau mdp </h1>
+  <?= $s_msg ?>
   <form  action="../controller/generatePwdC.php?token=<?= $i_token ?>"  method="post">
-    <input type="text" name="newPwd" placeholder="nouveau mot de passe"><br>
+    <input type="password" name="newPwd" placeholder="nouveau mot de passe"><br>
     <p> Confirmez votre mot de passe <br></p>
     <input type="password" name="confPwd" placeholder="confirmer mot de passe"><br>
     <input type="submit" name="action" value="envoyer"><br>
-    <?= $s_msg ?>
   </form>
 </main>
