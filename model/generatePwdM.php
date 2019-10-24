2623f592b457dd95f81df56ddee7d67ad76271bb
Laurent <laurent-vouriot@etu.univ-amu.fr>
2019-10-24 16:42:48 +0200

    modif add token

--- model/generatePwdM.php
+++ model/generatePwdM.php
@@ -34,8 +34,8 @@
 
     // met à jour  dans la BD le mot de passe de l'utilisateur qui à le même token passé en paramètre
     $query  = 'UPDATE User
-               SET password = \'' . $s_newPwd .'\''.'
-               WHERE token  = \'' . $i_token .'\'';
+               SET password = \'' . $s_newPwd .'\'
+               WHERE token  = \'' . $i_token . '\'';
 
     $result = testError($dbLink,$query);
 
