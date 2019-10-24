049bcd3bd88ab5d16ddbc1e03eb8e7b5dc9fd19f
leo-dalloz <leo.dalloz@etu.univ-amu.fr>
2019-10-24 16:38:38 +0200

    Finition du bouton + test de si la taille du nom de la discussion est nulle ou si supérieur à une valeur

--- controller/indexC.php
+++ controller/indexC.php
@@ -5,7 +5,11 @@ $s_action = $_POST['action'];
 if($s_action == 'createDiscussion')
 {
     $s_nomDiscussion = $_POST['nomDiscussion'];
-    createNewDiscussion($s_nomDiscussion);
+    $i_tailleDiscussion = strlen($s_nomDiscussion);
+    if ($i_tailleDiscussion > 1 && $i_tailleDiscussion < 20)
+    {
+        createNewDiscussion($s_nomDiscussion);
+    }
 }
 
 function getTabDiscussion()
