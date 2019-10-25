0b9061be26f88f3a64b36c7419b56d8b11fae1d1
Laurent <laurent-vouriot@etu.univ-amu.fr>
2019-10-25 22:30:11 +0200

    test date token

--- model/generatePwdM.php
+++ model/generatePwdM.php
@@ -20,7 +20,7 @@
         return false;
 
        $d_currentDate = new DateTime('now');
-       $d_dateToken   = DateTime::createFromFormat('Y-m-d H:i:s', $dbRow['dateToken']);
+       $d_dateToken   = DateTime::createFromFormat('Y-m-d H:i:s:u', $dbRow['dateToken']);
 
       //si la date du token est superieur à 15 min kaput
       if($d_currentDate->diff($d_dateToken)->format('%Y') < 1
