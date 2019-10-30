748adf575badbd71634a9561de375be2b8fa4ccf
Audrey Wagner <49270523+audreywagner@users.noreply.github.com>
2019-10-30 10:29:29 +0100

    modification vue profile

--- view/profileV.php
+++ view/profileV.php
@@ -1,7 +1,7 @@
 <?php
     require '../utils.inc.php';
 
-    require '../Model/User.php';
+    require '../model/User.php';
 
 
     session_start();
@@ -10,77 +10,103 @@
         die('Erreur d\'authentification');
     }
 
-    start_page('Profil');
+    $s_error = $_GET['error'];
+
+    if ($s_error == 'wrongPseudo')
+        $s_error = 1;
+    else if ($s_error == 'wrongEmail')
+        $s_error = 2;
+    else if ($s_error == 'samePseudo')
+        $s_error = 3;
+    else if ($s_error == 'sameEmail')
+        $s_error = 4;
+    else
+        $s_error = 0;
+
+    $title = 'Profil | Freenote';
+    $style = '../assets/css/profil.css';
+
+    ob_start();
 ?>
+
+<main>
+    <section id ="ProfileContainer">
+        <div id="ProfileTitle">
+            Profil de <?php echo $_SESSION['user']->getMyPseudo(); ?>
+        </div>
+
+            <?php
+		 
+                if(isset($s_error) && $s_error != 0) {
+                    echo '<div id="ErrorContainer">'.$s_error.'</div>';
+                }
+            ?>
+            <?php
+                if (isset($_GET['success'])) {
+                      echo '<div id="SuccessContainer">' .$_GET['success']. '</div>';
+                }
+            ?>
+        <div id="listProfile">
+            <div class="ProfileLine">
+                <p class="CategName">
+                    NOM
+                </p>
+                <p class="CategValue">
+                    <?php echo $_SESSION['user']->getMySurname(); ?>
+                </p>
+                <a href="/Freenote/view/modificationProfileV.php?action=surname" class="CategLink"><i class="fas fa-angle-right"></i></a>
+            </div>
+            <div class="ProfileLine">
+                <p class="CategName">
+                    Prénom
+                </p>
+                <p class="CategValue">
+                    <?php echo $_SESSION['user']->getMyName(); ?>
+                </p>
+                <a href="/Freenote/view/modificationProfileV.php?action=firstname" class="CategLink"><i class="fas fa-angle-right"></i></a>
+            </div>
+            <div class="ProfileLine">
+                <p class="CategName">
+                    PSEUDO
+                </p>
+                <p class="CategValue">
+                    <?php echo $_SESSION['user']->getMyPseudo(); ?> 
+                </p>
+                <a href="/Freenote/view/modificationProfileV.php?action=nickname" class="CategLink"><i class="fas fa-angle-right"></i></a>
+            </div>
+            <div class="ProfileLine">
+                <p class="CategName">
+                    ANNIVERSAIRE
+                </p>
+                <p class="CategValue">
+                    <?php echo $_SESSION['user']->getMyBirth(); ?>
+                </p>
+                <a href="/Freenote/view/modificationProfileV.php?action=birthday" class="CategLink"><i class="fas fa-angle-right"></i></a>
+            </div>
+            <div class="ProfileLine">
+                <p class="CategName">
+                    SEXE
+                </p>
+                <p class="CategValue">
+                    <?php echo $_SESSION['user']->getMyGender(); ?>
+                </p>
+                <a href="/Freenote/view/modificationProfileV.php?action=gender" class="CategLink"><i class="fas fa-angle-right"></i></a>
+            </div>
+            <div class="ProfileLine mailLine">
+                <p class="CategName">
+                    ADRESSE E-MAIL
+                </p>
+                <p class="CategValue">
+                    <?= $_SESSION['user']->getMyEmail(); ?>
+                </p>
+            </div>
+        </div>
+    </section>
+
+</main>
 
 
 <?php
-$s_error = $_GET['error'];
-
-if ($s_error == 'wrongPseudo')
-    $s_error = 1;
-else if ($s_error == 'wrongEmail')
-    $s_error = 2;
-else if ($s_error == 'samePseudo')
-    $s_error = 3;
-else if ($s_error == 'sameEmail')
-    $s_error = 4;
-else
-    $s_error = 0;
-?>
-
-
-
-    <h1>Votre profil</h1>
-
-    <form action="../Controller/profileC.php" method="post">
-        <p>
-            Votre prénom : <?php echo $_SESSION['user']->getMySurname(); ?> <br/>
-            <input type="text" name="Surname" placeholder="Changer votre prénom" /> <br/>
-            <br/>
-
-            Votre nom : <?php echo $_SESSION['user']->getMyName(); ?> <br/>
-            <input type="text" name="Name" placeholder="Changer votre nom" /> <br/>
-
-            <br/>
-
-            Votre pseudo : <?php echo $_SESSION['user']->getMyPseudo(); ?> <br/>
-            <input type="text" name="Pseudo" placeholder="Changer votre pseudo" /> <br/>
-            <?php if ($s_error == 1) echo 'Le pseudo que vous avez choisi est déjà utilisé.';
-                    else echo '';   ?>
-            <br/>
-
-            Votre date de naissance : <?php echo $_SESSION['user']->getMyBirth(); ?> <br/>
-            Changer votre date de naissance : <br/>
-            <input type="date" name="Birth" placeholder="Date de naissance"/> <br/>
-
-            <br/>
-
-            Pour changer votre mot de passe : <br/>
-            <input type="password" name="Pwd" placeholder="Changer votre mot de passe" /> <br/>
-
-            <br/>
-
-            Votre genre : <?php echo $_SESSION['user']->getMyGender(); ?> <br/>
-            <select name="Gender">
-                <option value="Homme">
-                    Homme
-                </option>
-                <option value="Femme">
-                    Femme
-                </option>
-                <option value="Autre">
-                    Autre
-                </option>
-            </select>
-            <br/>
-        </p>
-
-        <button type="submit" name="button" value="changeInfo"> Enregistrer </button> <br/>
-
-    </form>
-
-
-<?php
-    end_page();
+    $content = ob_get_clean();
+    require('../template.php');
 ?>
