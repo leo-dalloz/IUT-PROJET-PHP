8f7e6b2183a00f654bc093b7d37a8d84e769ccb3
jeremy-pouzargues <jeremy.pouzargues@etu.univ-amu.fr>
2019-10-30 11:00:31 -0700

    legers problemes réparés inshallah

--- controller/registerC.php
+++ controller/registerC.php
@@ -2,17 +2,20 @@
 
     if (isset($_POST['register']))
     {
-        if (isset($_POST['Surname']) AND isset($_POST['Name']) AND isset($_POST['Pseudo']) AND isset($_POST['Email']) AND isset($_POST['Birth']) AND isset($_POST['Pwd']) AND isset($_POST['Pwd1']) AND isset($_POST['Gender'])) {
-            $s_surname = $_POST['Surname'];
-            $s_name = $_POST['Name'];
-            $s_pseudo = $_POST['Pseudo'];
-            $s_email = $_POST['Email'];
-            $d_birth = $_POST['Birth'];
-            $s_pwd = $_POST['Pwd'];
-            $s_pwd1 = $_POST['Pwd1'];
-            $s_gender = $_POST['Gender'];
+        $s_surname = $_POST['Surname'];
+        $s_name = $_POST['Name'];
+        $s_pseudo = $_POST['Pseudo'];
+        $s_email = $_POST['Email'];
+        $d_birth = $_POST['Birth'];
+        $s_pwd = $_POST['Pwd'];
+        $s_pwd1 = $_POST['Pwd1'];
+        $s_gender = $_POST['Gender'];
 
-            if ($s_pwd != $s_pwd1) {
+        if ($s_surname != NULL AND $s_name != NULL AND $s_pseudo != NULL AND $s_email != NULL AND $d_birth != NULL AND $s_pwd != NULL AND $s_pwd1 != NULL AND $s_gender != NULL)
+        {
+
+            if ($s_pwd != $s_pwd1)
+            {
                 header('Location: ../view/registerV.php?error=pwd');
             }
             else
@@ -21,7 +24,8 @@
 
                 require '../model/registerM.php';
 
-                if (checkPseudo($s_pseudo) AND checkEmail($s_email)) {
+                if (checkPseudo($s_pseudo) AND checkEmail($s_email))
+                {
 
                     $s_pwd = password_hash($s_pwd, PASSWORD_DEFAULT);
 
