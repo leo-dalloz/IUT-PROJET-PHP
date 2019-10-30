0b8809e34dff6b84aa271602231ac20cd5edf461
leo-dalloz <leo.dalloz@etu.univ-amu.fr>
2019-10-30 19:39:20 +0100

    debug du template css

--- template.php
+++ template.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" href="./assets/css/template.css">
+    <link rel="stylesheet" href="../assets/css/template.css">
     <link id="ThemeStylesheet" rel="stylesheet" href="../assets/css/theme/night.css">
     <link rel="stylesheet" href="<?= $style ?>">
     <script src="https://kit.fontawesome.com/b18ab37082.js" crossorigin="anonymous"></script>
