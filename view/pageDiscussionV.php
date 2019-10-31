e10cd1bcde1a292c7ffc2abcbf7b2cd20b29fc69
leo-dalloz <leo.dalloz@etu.univ-amu.fr>
2019-10-31 11:35:28 +0100

    debug css index

--- view/pageDiscussionV.php
+++ view/pageDiscussionV.php
@@ -16,92 +16,62 @@ $style_theme = '../assets/css/theme/day.css';
 ob_start();
 ?>
 
-    <div id="LDiscussionContainer">
-        <div id="LDTitle">
-            DISCUSSIONS
-        </div>
-
-        <table id="LDiscussion">
+<main>
+    <section id="ListConversationContainer">
+        <p id="ListConversation">
+            Conversations
+        </p>
+        <div id="LConvContainer">
             <?php foreach ($tab_discussions as $value) { ?>
-                <tr class="Discussion">
-                    <td>
-                        <a href="pageDiscussionV.php?discussionId=<?= $value->getDiscussionId()?>&etat=0" class="LinkDiscu"><?= $value->getName()?></a>
-                    </td>
-                    <td>
-                        <?= $value->getNbMessages() ?> Messages
-                    </td>
-                    <td>
-                        <?= $value->getNbMaxWords()?> Mots MAX
-                    </td>
-                    <td>
-                        <?php $b_isOuvert = $value->getState()?>
-                        <p class="<?= ($b_isOuvert) ? 'Open' : 'Close' ?>"><?= ($b_isOuvert) ? 'Ouverte' : 'Fermée' ?></p>
-                    </td>
-                    <td>
-                        <?= $value->getNbLike() ?> <i class="fas fa-thumbs-up"></i>
-                    </td>
-                </tr>
-            <?php } ?>
-        </table>
-    </div>
-    <main id="top">
-        <section id="FirstPartContainer">
-            <div id="DescriptionContainer">
-                <p id="DescriptionTitle">
-                    Bienvenue sur FreeNote
-                </p>
+                <button class="buttonLConv" onclick="window.location.href='pageDiscussionV.php?discussionId=<?= $value->getDiscussionId()?>&etat=0'">
+                    <ul class="LConv">
+                        <div class="FirstLine">
+                            <li>
+                                <a href="pageDiscussionV.php?discussionId=<?= $value->getDiscussionId()?>&etat=0" class="LinkDiscu"><?= $value->getName()?></a>
+                            </li>
+                            <li>
+                                <?= $value->getNbMessages() ?> Messages
+                            </li>
+                        </div>
+                        <div class="SecondLine">
+                            <li>
+                                <?= $value->getNbMaxWords()?> Mots MAX
+                            </li>
+                            <li>
+                                <?php $b_isOuvert = $value->getState()?>
+                                <p class="<?= ($b_isOuvert) ? 'Open' : 'Close' ?>"><?= ($b_isOuvert) ? 'Ouverte' : 'Fermée' ?></p>
+                            </li>
+                            <li >
+                                <?= $value->getNbLike() ?> <i class="fas fa-thumbs-up"></i>
+                            </li>
+                        </div>
+                    </ul>
+                </button>
+            <?php }?>
+        </div>
+    </section>
 
-                <p id="DescriptionText">
-                    FreeNote est un réseau social d’un nouveau genre qui consiste à créer des fils de discussions comprenant
-                    des messages participatifs au sein desquels chaque utilisateur ne peut ajouter qu’un ou deux mots.
-                    <br> <br> <b> Inscrivez-vous vite ! </b>
-                </p>
+    <section id="conversationContainer">
+        <?php $Messages = $D_discussion->getMessages();
+        foreach ($Messages as $message) {
+            ?>
+            <div id="listMessage">
+                <div class="line">
+                    <p class="message">
+                        <?= $message->getContents(); ?>
+                    </p>
+                </div>
             </div>
-        </section>
+        <?php } ?>
+        <div id="SendMessageContainer">
+            <form id="formMessage" action="../controller/pageDiscussionC.php?discussionId=<?=$i_discussionId?>" method="post" >
+                <input id="contentInput" type="text" name="contents" placeholder="Message">
+                <button id="submitButton" type="submit" name="action" value="sendMessage"><i class="fab fa-telegram-plane"></i></button>
+            </form>
+        </div>
+    </section>
+</main>
 
-        <section id="secondPartSection">
-            <div id="listArg">
-                <p class="Arg">
-                    <i class="fas fa-users"></i>
-                    Rencontrez des Freenoter
-                </p>
-                <p class="Arg">
-                    <i class="fab fa-readme"></i>
-                    Parcourez les discussions
-                </p>
-                <p class="Arg">
-                    <i class="fas fa-comments"></i>
-                    Répondez aux discussions
-                </p>
-                <p class="Arg">
-                    <i class="fas fa-times-circle"></i>
-                    Mais pas plus de 2 mots
-                </p>
-            </div>
-
-
-        </section>
-        <section id="DescriptionButtons">
-            <a class="MainButton" href="">S'inscrire</a>
-            <a class="MainButton" href="">Se connecter</a>
-        </section>
-        <section id="responsiveSection">
-            <img src="../assets/images/telThemeClair.png" id="responsiveTel"alt="tel image mobile">
-            <p id="responsiveText">
-                Disponible sur téléphone ! <br>
-                Disponible en thème foncé, <br>
-                et clair !
-            </p>
-        </section>
-    </main>
-
-    <div id="returnTopContainer">
-        <a href="#top">
-            <i class="far fa-arrow-alt-circle-up"></i>
-        </a>
-    </div>
-
-    <script src="../assets/js/index.js"></script>
 <?php
 $content = ob_get_clean();
 require ('../template.php');
