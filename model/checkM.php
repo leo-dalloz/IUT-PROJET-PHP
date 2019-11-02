a843ad2018ec70e27cfbc4c10ac25b5951abfef3
leo-dalloz <leo.dalloz@etu.univ-amu.fr>
2019-11-02 17:31:09 +0100

    debug

--- model/checkM.php
+++ model/checkM.php
@@ -5,20 +5,24 @@ function checkPseudo($s_pseudo)
     $dbLink = dbConnect();
     $query = 'SELECT pseudo FROM `User` WHERE pseudo = \'' . $s_pseudo . '\'';
     $dbResult = testError($dbLink,$query);
-    $result = $dbResult->fetch_assoc();
-    if ($result['pseudo'] != NULL)
-        return 0;
-    else
-        return 1;
+    while($dbRow = mysqli_fetch_assoc($dbResult))
+    {
+        if ($dbRow['pseudo'] != NULL)
+            return 0;
+        else
+            return 1;
+    }
 }
 function checkEmail($s_email)
 {
     $dbLink = dbConnect();
     $query = 'SELECT email FROM `User` WHERE email = \'' . $s_email . '\'';
     $dbResult = testError($dbLink,$query);
-    $result = $dbResult->fetch_assoc();
-    if ($result['email'] != NULL)
-        return 0;
-    else
-        return 1;
+    while($dbRow = mysqli_fetch_assoc($dbResult))
+    {
+        if ($dbRow['email'] != NULL)
+            return 0;
+        else
+            return 1;
+    }
 }
