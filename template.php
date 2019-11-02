f97a03e16db420ccc4b5d7e5f74a7f9d3757ba3f
leo-dalloz <leo.dalloz@etu.univ-amu.fr>
2019-11-02 15:00:18 +0100

    debug css

--- template.php
+++ template.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" href="assets/css/template.css">
+    <link rel="stylesheet" href="../assets/css/template.css">
     <link id="ThemeStylesheet" rel="stylesheet" href="../assets/css/theme/night.css">
 
     <!-- Custom page css link -->
