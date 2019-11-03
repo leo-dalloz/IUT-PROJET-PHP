35793d843809b5aa85d791be3afdd0ec1c5da382
jeremy-pouzargues <jeremy.pouzargues@etu.univ-amu.fr>
2019-11-03 12:44:47 -0800

    dernieres finalisations

--- model/checkM.php
+++ model/checkM.php
@@ -6,10 +6,7 @@ function checkPseudo($s_pseudo)
     $query = 'SELECT pseudo FROM `User` WHERE pseudo = \'' . $s_pseudo . '\'';
     $dbRow = testError($dbLink,$query);
     $dbResult = $dbRow->fetch_assoc();
-    if ($dbResult['pseudo'] != NULL)
-        return false;
-    else
-        return true;
+    return false;
 }
 function checkEmail($s_email)
 {
