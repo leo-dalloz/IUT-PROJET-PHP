4d69c15c35cbd526de3ba617704f9218dc4845b7
laurent-vouriot <laurent.vouriot@etu.univ-amu.fr>
2019-11-03 21:08:10 +0100

    modifs

--- view/newPwdV.php
+++ view/newPwdV.php
@@ -1,26 +1,29 @@
 <?php
-      $title = 'Nouveau mot de passe';
-      $style = 'StyleNewPassWord.css';
-
-      $step = $_GET['step'];
-      if ($step == null)
-          $msg = 'verification mail';
-      else if($step == 'ok')
-          $msg = 'un mail à été envoyé ';
-      elseif ($step == 'error')
-          $msg = 'adresse mail inexistante dans notre base de données ';
-
+      $title = 'Mot de passe oublié';
+      $style_common = '../assets/css/connexion-inscription.css';
+      $style = '../assets/css/newPwd.css';
+      ob_start()
 ?>
 
-
-<main>
-  <h1> Mot de passe oublié </h1>
-  <h2> <?= $msg ?> </h2>
-  <form  action = "../controller/newPwdC.php" method="post">
-      Veuillez entrer votre adresse email <br>
-      <input type="text" name="mail" value="mail" placeholder="mail"> <br>
-      <input type="submit" name="action" value="envoyer"> <br>
-  </form>
-
-
-</main>
+      <main>
+        <div id="LogoMainContainer">
+            <img src="../assets/images/test2.png" id="LogoMain">
+        </div>
+        <div id="PageTitleContainer">
+          <h1 id="PageTitle"> Mot de passe oublié </h1>
+          <h2 id="message"> <?= $msg ?>   </h2>
+        </div>
+        <div id="FormMailContainer">
+          <form  action = "../controller/newPwdC.php" method="post" id="FormMail">
+              Veuillez entrer votre adresse email <br>
+              <input type="text" name="mail" placeholder="mail"> <br>
+              <div id="submitContainer">
+                <input type="submit" name="action" value="envoyer" id="submitButton"> <br>
+              </div>
+          </form>
+        </div>
+      </main>
+<?php
+      $content = ob_get_clean();
+      require('../template_empty.php');
+?>
