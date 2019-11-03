e8bf57abb6d0f825636dab8a1427570cfd5704aa
laurent-vouriot <laurent.vouriot@etu.univ-amu.fr>
2019-11-03 19:12:26 +0100

    debug  inscription

--- model/checkM.php
+++ model/checkM.php
@@ -5,13 +5,13 @@ function checkPseudo($s_pseudo)
     $dbLink = dbConnect();
     $query = 'SELECT pseudo FROM User WHERE pseudo = \'' . $s_pseudo . '\'';
     $dbResult = testError($dbLink,$query);
-    //while($dbRow = mysqli_fetch_assoc($dbResult))
-    //{
+    while($dbRow = mysqli_fetch_assoc($dbResult))
+    {
         if ($dbRow['pseudo'] == NULL)
             return true;
         else
             return false;
-    //}
+    }
 }
 function checkEmail($s_email)
 {
