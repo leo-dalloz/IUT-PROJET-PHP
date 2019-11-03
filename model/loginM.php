df82ebac87540ddc8851da5b2e941ec39434222f
laurent-vouriot <laurent.vouriot@etu.univ-amu.fr>
2019-11-03 17:03:34 +0100

    modif coquille

--- model/loginM.php
+++ model/loginM.php
@@ -1,37 +1,37 @@
 <?php
-<<<<<<< HEAD
 
 
-    require 'User.php';
+    //
+    // require 'User.php';
+    //
+    // require '../base.php';
+    //
+    //
+    // function login($s_pseudo,$s_pwd)
+    // {
+    //     $dbLink = dbConnect();
+    //
+    //
+    //     $query = 'SELECT pseudo AS pseudo, password AS password FROM `User` WHERE pseudo = \'' . $s_pseudo . '\'';
+    //
+    //     if (!($dbResult = mysqli_query($dbLink, $query))) {
+    //         echo 'Erreur de requête<br/>';
+    //         //Affiche le type d'erreur.
+    //         echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+    //         //Affiche la requête envoyée.
+    //         echo 'Requête : ' . $query . '<br/>';
+    //         exit();
+    //     }
+    //
+    //     $resultat = $dbResult->fetch_assoc();
+    //
+    //
+    //     if (password_verify($s_pwd, $resultat['password']) AND $s_pwd != NULL AND $resultat['pseudo'] == $s_pseudo AND $s_pseudo != NULL)
+    //         return true;
+    //     else
+    //         return false;
 
-    require '../base.php';
 
-
-    function login($s_pseudo,$s_pwd)
-    {
-        $dbLink = dbConnect();
-
-
-        $query = 'SELECT pseudo AS pseudo, password AS password FROM `User` WHERE pseudo = \'' . $s_pseudo . '\'';
-
-        if (!($dbResult = mysqli_query($dbLink, $query))) {
-            echo 'Erreur de requête<br/>';
-            //Affiche le type d'erreur.
-            echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
-            //Affiche la requête envoyée.
-            echo 'Requête : ' . $query . '<br/>';
-            exit();
-        }
-
-        $resultat = $dbResult->fetch_assoc();
-
-
-        if (password_verify($s_pwd, $resultat['password']) AND $s_pwd != NULL AND $resultat['pseudo'] == $s_pseudo AND $s_pseudo != NULL)
-            return true;
-        else
-            return false;
-
-=======
 require 'User.php';
 require 'dbTest.php';
 function login($s_pseudo,$s_pwd)
@@ -45,7 +45,7 @@ function login($s_pseudo,$s_pwd)
         //Affiche la requête envoyée.
         echo 'Requête : ' . $query . '<br/>';
         exit();
->>>>>>> finalJeremyDevelop
+
     }
     $resultat = $dbResult->fetch_assoc();
     if (password_verify($s_pwd, $resultat['password']) AND $s_pwd != NULL AND $resultat['pseudo'] == $s_pseudo AND $s_pseudo != NULL)
@@ -65,11 +65,9 @@ function returnUser ($s_pseudo)
         echo 'Requête : ' . $query . '<br/>';
         exit();
     }
-<<<<<<< HEAD
-=======
-    $result = $dbResult->fetch_assoc();
+
+    $dbResult->fetch_assoc();
     $myUser = new User($result['admin'], $result['surname'], $result['name'], $result['pseudo'],$result['email'], $result['birthdate'], $result['password'],$result['gender']);
     $myUser->setMyId($result['id']);
     return $myUser;
 }
->>>>>>> finalJeremyDevelop
