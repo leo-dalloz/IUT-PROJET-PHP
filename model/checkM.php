de439fc8955d3389fa7afa3e421357fe237c039f
jeremy-pouzargues <jeremy.pouzargues@etu.univ-amu.fr>
2019-11-03 12:31:19 -0800

    dernieres finalisations

--- model/checkM.php
+++ model/checkM.php
@@ -5,30 +5,22 @@ function checkPseudo($s_pseudo)
     $dbLink = dbConnect();
     $query = 'SELECT pseudo FROM User WHERE pseudo = \'' . $s_pseudo . '\'';
     $dbRow = testError($dbLink,$query);
-  //  while($dbRow = mysqli_fetch_assoc($dbResult))
-  //  {
-    $dbResult = $dbRow->fetch_assoc();
-    print_r($dbResult);
-    exit();
-        if ($dbResult['pseudo'] == NULL)
-            return true;
-        else
-            return false;
-    //}
+  while($dbRow = mysqli_fetch_assoc($dbRow))
+  {
+      return false;
+  }
+    return true;
 }
 function checkEmail($s_email)
 {
     $dbLink = dbConnect();
     $query = 'SELECT email FROM User WHERE email = \'' . $s_email . '\'';
     $dbRow = testError($dbLink,$query);
-    //while($dbRow = mysqli_fetch_assoc($dbResult))
-    //{
-    $dbResult = $dbRow->fetch_assoc();
-        if ($dbResult['email'] != NULL)
-            return 0;
-        else
-            return 1;
-    //}
+    while($dbRow = mysqli_fetch_assoc($dbRow))
+    {
+        return false;
+    }
+    return true;
 }
 
 /*
