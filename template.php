af428f9594bb6b56d792aef409c8d1d7b642c68e
Audrey Wagner <49270523+audreywagner@users.noreply.github.com>
2019-11-03 23:12:28 +0100

    ajout favicon

--- template.php
+++ template.php
@@ -9,7 +9,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-        <link rel="icon" type="image/png" href="./assets/images/freenoteWWings.png" />
+        <link rel="icon" type="image/png" href="../assets/images/LogoThemeFonce.png" />
 
 	    
         <link rel="stylesheet" href="../assets/css/template.css">
