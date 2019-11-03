e4a4b4e4225d75dfd4b2e2603055305012c70eae
Audrey Wagner <49270523+audreywagner@users.noreply.github.com>
2019-11-03 23:02:30 +0100

    Update template.php

--- template.php
+++ template.php
@@ -4,6 +4,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        
+        <link rel="icon" type="image/png" href="./assets/images/freenoteWWings.png" />
 
         <link rel="stylesheet" href="<?= $style ?>">
         <link id="ThemeStylesheet" rel="stylesheet" href="<?= $style_theme ?>">
