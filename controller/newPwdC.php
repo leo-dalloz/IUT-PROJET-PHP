d27d278b8d21076b9f7aa58917e46e279e23e836
laurent-vouriot <laurent.vouriot@etu.univ-amu.fr>
2019-11-03 20:37:42 +0100

    pb merge

--- controller/newPwdC.php
+++ controller/newPwdC.php
@@ -1,47 +1,48 @@
 <?php
-
-
-    require('../model/newPwdM.php');
-
-    $s_mail = $_POST['mail'];
-    /**
-    * Laurent
-    * genere un token aléatoire de 6 chiffres
-    * out : int
-    */
-    function generateToken() {
-      $i_token = rand(100000,999999);
-      return $i_token;
-    } //generateToken()
+    require ('../model/newPwdM.php');
     /*
     * Laurent
     * Envoie un mail avec le token généré à l'utilisateur souhaitant changer son mot de passe
     * in : string mail de user
     */
     function sendMail($s_mail) {
-
-      $i_token = generateToken();
-      $s_obj = 'mot de passe oublié' ;
-      $s_msg = 'bonjour cliquez sur le <a href="generatePass.php?token='. $i_token . '">lien</a> svp';
-
-
-      addToken($i_token,$s_mail);
-      mail('laurent.vourio@etu.univ-amu.fr','test','test');
-      if(mail($s_mail,$s_obj,$s_msg))
-      {
-        print_r('ok');
-        exit();
-      }
-
+      $s_token  = md5(uniqid(mt_rand(100000,999999)));
+      $boundary = uniqid(mt_rand(1,1000));
+      $mail1    = '../view/email1.html';
+      $mail2    = '../view/email2.html';
+      $handle1  = fopen($mail1,'r');
+      $handle2  = fopen($mail2,'r');
+      $headers  = "MIME-Version: 1.0\r\n";
+      $s_obj   .= "mot de passe oublié";
+      $headers .= "Content-Type: multipart/alternative;boundary=" . $boundary . "\r\n";
+      // mail au format texte
+      $message .= "\r\n\r\n--" . $boundary . "\r\n";
+      $message .= "Content-type: text/plain;charset=utf-8\r\n\r\n";
+      $message .= "mot de passe oublié ? cliquez sur ce lien : \n";
+      $message .= 'http://projet-iut-info.alwaysdata.net/mdpoublie/PROJET-PHP/controller/generatePwdC.php?token=' . $s_token . '&step=hello';
+      //    mail au format html
+      $message .= "\r\n\r\n--" . $boundary . "\r\n";
+      $message .= "Content-type: text/html;charset=utf-8\r\n\r\n";
+      $message .= fread($handle1,filesize($mail1));
+      $message .= 'http://projet-iut-info.alwaysdata.net/mdpoublie/PROJET-PHP/controller/generatePwdC.php?token=' . $s_token . '&step=hello <br><br>';
+      $message .= fread($handle2,filesize($mail2));
+      $message .= "\r\n\r\n--" . $boundary . "--";
+      addToken($s_token,$s_mail);
+      mail($s_mail,$s_obj,$message, $headers);
+      fclose($handle1);
+      fclose($handle2);
     } // sendMail()
-
-    // si le mail existe dans la bd on averti l'utilisateur de l'envoie du mail sinon en le previent de la non existance du mail
-    if(getMail($s_mail))
-    {
-      sendMail($s_mail);
-      header('Location: ../view/ViewNewPwd.php?step=ok');
+    
+    if (isset($_POST['mail'])) {
+      $s_mail = $_POST['mail'];
+      // si le mail existe dans la bd on averti l'utilisateur de l'envoie du mail sinon en le previent de la non existance du mail
+      if(getMail($s_mail))
+      {
+        sendMail($s_mail);
+        header('Location: ../view/newPwdV.php?step=ok');
+      }
+      else {
+        header('Location: ../view/newPwdV.php?step=error');
+      }
     }
-    else {
-
-      header('Location: ../view/ViewNewPwd.php?step=error');
-    }
+    require ('../view/newPwdV.php');
