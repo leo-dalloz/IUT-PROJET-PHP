85b5805d4e3d6ebf36f558ce74eed19adb9f1703
jeremy-pouzargues <jeremy.pouzargues@etu.univ-amu.fr>
2019-11-03 13:10:16 -0800

    dernieres finalisations

--- template.php
+++ template.php
@@ -28,7 +28,7 @@
                         <a class="navLink" href="../controller/loginC.php">Connexion <i class="fas fa-sign-in-alt"></i></a>
                         <a class="navLink" href="../controller/registerC.php">Inscription <i class="fas fa-user-plus"></i></a>
                     <?php } else { ?>
-                        <a class="navLink" href="../view/basicProfileV.php">Profil</a>
+                        <a class="navLink" href="../controller/profileC.php">Profil</a>
                         <a class="navLink" href="../controller/deconnectionC.php">Deconnexion <i class="fas fa-sign-out-alt"></i></a>
                     <?php } 
                         if ($isAdmin) {
