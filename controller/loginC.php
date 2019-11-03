bccdee62293f3088e309d296bd7832fe7f6ef90a
laurent-vouriot <laurent.vouriot@etu.univ-amu.fr>
2019-11-03 18:24:21 +0100

    modif pb de merge

--- controller/loginC.php
+++ controller/loginC.php
@@ -1,20 +1,38 @@
 <?php
   session_start();
   require '../model/loginM.php';
-  $s_pseudo = $_POST['Pseudo'];
-  $s_password = $_POST['Pwd'];
-  
-  if (login($s_pseudo,$s_password))
+  if (isset($_POST['login']))
   {
-    $_SESSION['login'] = 'ok';
-    $_SESSION['user'] = returnUser($s_pseudo);
-    if ($_SESSION['user']->getMyAdmin() == 1)
-      header('Location: ../view/pageTestV.php?admin=oui');
-    else
-      header('Location: ../view/pageTestV.php?admin=non');
+    if (isset($_POST['Pseudo']) AND isset($_POST['Pwd']))
+    {
+        $s_pseudo = $_POST['Pseudo'];
+        $s_password = $_POST['Pwd'];
+        if (login($s_pseudo, $s_password))
+        {
+            $_SESSION['login'] = 'ok';
+            $_SESSION['user'] = returnUser($s_pseudo);
+            header('Location: ../controller/indexC.php');
+        }
+        else
+            header('Location: ../controller/loginC.php?step=error');
     }
+    else
+        header('Location: ../controller/indexC.php?step=missing');
+  }
+  if (isset($_GET['step']))
+  {
+    if ($_GET['step'] == 'error')
+        $s_error = 'L\'identifiant et le mot de passe ne correspondent pas.';
+    else if ($_GET['step'] == 'missing')
+        $s_error = 'Vous avez oublié de remplir un des champs.';
+    else
+        $s_error = '';
+  }
   else
-    header('Location: ../view/loginV.php?step=error');
+    $s_error = '';
+  require '../view/loginV.php';
+
+  
 /*
 session_start();
 require '../model/loginM.php';
