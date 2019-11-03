49cc1aa5186b84b076baeefada3d96063795e75e
Audrey Wagner <49270523+audreywagner@users.noreply.github.com>
2019-11-03 23:18:49 +0100

    ajout validation W3C toutes les pages css

--- template.php
+++ template.php
@@ -54,6 +54,13 @@
                     </p>
                 </div>
             </section>
+            <p>
+                <a href="http://jigsaw.w3.org/css-validator/check/referer">
+                    <img style="border:0;width:88px;height:31px"
+                    src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+                    alt="CSS Valide !" />
+                </a>
+            </p>
         </footer>
     </body>
     <script src="./assets/js/architecture.js"></script>
