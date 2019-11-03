d2f2860532b60181543c226aa21e3fa83f42c2b0
leo-dalloz <leo.dalloz@etu.univ-amu.fr>
2019-11-03 23:26:13 +0100

    debug popup

--- controller/pageDiscussionC.php
+++ controller/pageDiscussionC.php
@@ -29,11 +29,9 @@ if (isset($_POST['action']))
         {
             header('Location: ../controller/pageDiscussionC.php?error=' . 'Déja participé' . '&discussionId=' . $i_discussionId);
         }
-        else if (str_word_count($s_contents, 0, '123456789\'') <= $D_discussion::NbMaxWords
+        else if (str_word_count($s_contents, 0, '123456789') <= $D_discussion::NbMaxWords
             && str_word_count($s_contents, 0, '123456789') != 0) {
-//            if (strpos($s_contents, '\'') == TRUE) {
-//                $D_discussion->closeAMessage();
-//            }
+            $s_contents = addslashes($s_contents);
             if (-1 == $i_lastMessageID) {
                 if (1 == $D_discussion->canOpenDiscussion()) {
                     createMessage($i_discussionId);
