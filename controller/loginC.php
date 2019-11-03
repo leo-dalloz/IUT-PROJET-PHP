466b6e42d7b51a5a71098448ad193d693a6825f2
Audrey Wagner <49270523+audreywagner@users.noreply.github.com>
2019-11-03 21:40:31 +0100

    ajout popup

--- controller/loginC.php
+++ controller/loginC.php
@@ -1,33 +1,22 @@
 <?php
-  session_start();
-  require '../model/loginM.php';
-  if (isset($_POST['login']))
-  {
-    if (isset($_POST['Pseudo']) AND isset($_POST['Pwd']))
+
+    session_start();
+
+    require '../model/loginM.php';
+
+    $s_pseudo = $_POST['Pseudo'];
+    $s_password = $_POST['Pwd'];
+
+
+    if (login($s_pseudo,$s_password))
     {
-        $s_pseudo = $_POST['Pseudo'];
-        $s_password = $_POST['Pwd'];
-        if (login($s_pseudo, $s_password))
-        {
-            $_SESSION['login'] = 'ok';
-            $_SESSION['user'] = returnUser($s_pseudo);
-            header('Location: ../controller/indexC.php');
-        }
+        $_SESSION['login'] = 'ok';
+        $_SESSION['user'] = returnUser($s_pseudo);
+        $_SESSION['popupsuccess'] = 'Vous êtes maintenant connecté';
+        if ($_SESSION['user']->getMyAdmin() == 1)
+            header('Location: ../view/indexV.php');
         else
-            header('Location: ../controller/loginC.php?step=error');
+            header('Location: ../view/indexV.php');
     }
     else
-        header('Location: ../controller/indexC.php?step=missing');
-  }
-  if (isset($_GET['step']))
-  {
-    if ($_GET['step'] == 'error')
-        $s_error = 'L\'identifiant et le mot de passe ne correspondent pas.';
-    else if ($_GET['step'] == 'missing')
-        $s_error = 'Vous avez oublié de remplir un des champs.';
-    else
-        $s_error = '';
-  }
-  else
-    $s_error = '';
-  require '../view/loginV.php';
+        header('Location: ../view/loginV.php?step=error');
